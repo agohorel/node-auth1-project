271a06bd1cd34168a9220ee2e8c272fbd5edaffa
agohorel <gohorelalex@gmail.com>
2020-02-25 17:57:33 -0500

    basic client functionality (register/login/redirect)

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import { Form } from "./components/Form";
+import { TopSecret } from "./components/TopSecret";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Route path="/login">
+          <Form type="login"></Form>
+        </Route>
+        <Route path="/register">
+          <Form type="register"></Form>
+        </Route>
+        <Route path="/top-secret">
+          <TopSecret></TopSecret>
+        </Route>
+      </Router>
+    </>
   );
 }
 
