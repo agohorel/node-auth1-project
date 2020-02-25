46bc9cfaf5608c1857fdd417a70348dd5ca5fec2
agohorel <gohorelalex@gmail.com>
2020-02-25 16:39:48 -0500

    add session boilerplate, setSession helper, and logout route

--- server.js
+++ server.js
@@ -1,14 +1,36 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
+const session = require("express-session");
+const KnexStore = require("connect-session-knex")(session);
+
 const db = require("./users/user-model.js");
 const validateCreds = require("./middleware/validateCreds.js");
-const validateUser = require("./middleware/validateUser.js");
 const userRouter = require("./users/users-router.js");
+const knexConfig = require("./data/dbConfig.js");
 
 const server = express();
 const port = 5000;
+const sessionConfig = {
+  name: "session",
+  secret: "quakeisbetterthandoomfightme",
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    maxAge: 1000 * 60 * 30,
+    secure: false,
+    httpOnly: true
+  },
+  store: new KnexStore({
+    knex: knexConfig,
+    tablename: "sessions",
+    createTable: true,
+    sidfieldname: "sid",
+    clearInterval: 1000 * 60 * 30
+  })
+};
 
 server.use(express.json());
+server.use(session(sessionConfig));
 server.use("/restricted", userRouter);
 
 server.post("/register", validateCreds, async (req, res) => {
@@ -17,6 +39,7 @@ server.post("/register", validateCreds, async (req, res) => {
     const hashed = bcrypt.hashSync(password, 14);
     password = hashed;
     await db.insert({ username, password });
+    setSession(req, username);
     res.status(201).json({ msg: `welcome ${username}!` });
   } catch (error) {
     if (error.errno === 19) {
@@ -26,14 +49,37 @@ server.post("/register", validateCreds, async (req, res) => {
   }
 });
 
-server.post("/login", [validateCreds, validateUser], async (req, res) => {
+server.post("/login", validateCreds, async (req, res) => {
   try {
     const userData = await db.findBy({ username: req.body.username });
-    res.cookie("user_id", userData.id);
+    setSession(req, userData.username, userData.id);
     res.status(200).json({ msg: `welcome, ${req.body.username}!` });
   } catch (error) {
     res.status(500).json({ error });
   }
 });
 
+server.get("/logout", async (req, res) => {
+  try {
+    if (req.session) {
+      req.session.destroy(err => {
+        if (err) {
+          res.status(500).json({ msg: "failed to logout, please try again" });
+        } else {
+          res.status(200).json({ msg: "successfully logged out!" });
+        }
+      });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "server error :(" });
+  }
+});
+
 server.listen(port, () => console.log(`server listening on port ${port}`));
+
+function setSession(req, username, id) {
+  req.session.loggedIn = true;
+  req.session.username = username;
+  req.session.userID = id;
+  return req;
+}
