34537b33a21e749b9a4e90ecebce50fdd69df7ee
agohorel <gohorelalex@gmail.com>
2020-02-25 16:38:56 -0500

    improve validateUser middleware

--- middleware/validateUser.js
+++ middleware/validateUser.js
@@ -1,28 +1,7 @@
-const bcrypt = require("bcryptjs");
-const db = require("../users/user-model.js");
-
-module.exports = async (req, res, next) => {
-  let user, pass;
-  if (req.headers.username && req.headers.password) {
-    user = req.headers.username;
-    pass = req.headers.password;
+module.exports = (req, res, next) => {
+  if (req.session && req.session.loggedIn) {
+    next();
   } else {
-    user = req.body.username;
-    pass = req.body.password;
-  }
-
-  if (user && pass) {
-    try {
-      const userData = await db.findBy({ username: user }).first();
-      if (userData && bcrypt.compareSync(pass, userData.password)) {
-        next();
-      } else {
-        res.status(401).json({ msg: "You shall not pass!" });
-      }
-    } catch (error) {
-      res.status(500).json({ error });
-    }
-  } else {
-    res.status(400).json({ msg: "please provide valid credentials" });
+    res.status(401).json({ message: "you shall not pass" });
   }
 };
